455ef6d4e6c92c1a035cb0874a198b9f35400222
Gorzerk-Vip <sql_network@163.com>
2020-09-14 11:07:58 +0800

    debug

--- backschool/src/main/java/cn/edu/zufe/SyncHzjkm.java
+++ backschool/src/main/java/cn/edu/zufe/SyncHzjkm.java
@@ -11,15 +11,15 @@ public class SyncHzjkm {
 	public static void main(String[] args){
 		Connection connection = DBUtils.getConnection();
 		try {
-			HttpInterface httpInterface = new HttpInterface();
+			//HttpInterface httpInterface = new HttpInterface();
 			//httpInterface.doPostOrGet(pathUrl, data);
 			Statement st = connection.createStatement();
-			String querySql = "select xh from icdc_ehall.flow_yjs_back_school where xh  is not null";
-			String updateSql = "";
+			String querySql = "select * from icdc_xx.hzjkm where mzt is null";
+			//String updateSql = "";
 			ResultSet xghs = st.executeQuery(querySql);
 			while(xghs.next()){
-				String xh = xghs.getString("xh");
-				
+				String xh = xghs.getString("xgh");
+				System.out.println("xh: " + xh);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
